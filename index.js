15e20c3d46d3810db77064347d776e191832edb6
Claire Iidea <danbua999@gmail.com>
2026-03-25 20:44:29 +0700

    fix some stuffs and add more things

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Main = document.getElementById("main"),
-    CB = document.getElementById("chickenbutt")
+    CB = document.getElementById("chickenbutt"),
+    Age = document.getElementById("age")
 ;
 
 const CustomHyperLinks = document.getElementsByClassName("custom-links");
@@ -9,6 +10,18 @@ const CustomHyperLinks = document.getElementsByClassName("custom-links");
  */
 let CBCoolDown = null;
 
+const GetYearsSince = (epoch) => {
+    const StartDay = new Date(epoch);
+    const Now = new Date();
+    const Years = Now.getFullYear() - StartDay.getFullYear();
+    const HasHadAnniversary = Now.getMonth() > StartDay.getMonth() || 
+        (Now.getMonth() === StartDay.getMonth() && Now.getDate() >= StartDay.getDate());
+    
+    return HasHadAnniversary ? Years : Years - 1;
+};
+
+Age.textContent = GetYearsSince(new Date(2007, 0, 1, 0, 0, 0, 0).getTime());
+
 CB.addEventListener("click", () => {
     if(CBCoolDown) 
         clearTimeout(CBCoolDown);
@@ -17,7 +30,7 @@ CB.addEventListener("click", () => {
     CBCoolDown = setTimeout(() => {
         CB.textContent = "guess what?";
         CBCoolDown = null;
-    }, 3000);
+    }, 1000);
 });
 
 for(const Link of CustomHyperLinks) {
