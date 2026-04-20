ac3cd2338dc5b5eb85e7e56728f4f08a4c943fb9
Claire Iidea <danbua999@gmail.com>
2026-04-20 14:42:38 +0700

    slightly changes code

--- potfolio/src/components/RenderColor.tsx
+++ potfolio/src/components/RenderColor.tsx
@@ -1,11 +1,22 @@
 import type { JSX } from "react";
 
-const RenderColor = (...Items: [string, string][]): JSX.Element => {
-    if(Items.length === 1) {
-        const [Text, Color]: [string, string] = Items[0];
-        return (
-            <span 
-                className="transition-colors duration-[.3s] hover:text-[color:var(--hover-color)]" 
+const RenderColor = (...Items: [string, string][]): JSX.Element => Items.length === 1 ? 
+    <span 
+        className="transition-colors duration-[.3s] hover:text-[color:var(--hover-color)]" 
+        style={
+            {
+                "--hover-color": Items[0][1]
+            } as React.CSSProperties
+        }
+    >
+        {Items[0][0]}
+    </span>
+    :
+    <span className="group">
+        {Items.map(([Text, Color]: [string, string], Index: number): JSX.Element => (
+            <span
+                key={Index}
+                className="transition-colors duration-[.3s] group-hover:text-[color:var(--hover-color)]"
                 style={
                     {
                         "--hover-color": Color
@@ -14,26 +25,8 @@ const RenderColor = (...Items: [string, string][]): JSX.Element => {
             >
                 {Text}
             </span>
-        );
-    }
-
-    return (
-        <span className="group">
-            {Items.map(([Text, Color]: [string, string], Index: number): JSX.Element => (
-                <span
-                    key={Index}
-                    className="transition-colors duration-[.3s] group-hover:text-[color:var(--hover-color)]"
-                    style={
-                        {
-                            "--hover-color": Color
-                        } as React.CSSProperties
-                    }
-                >
-                    {Text}
-                </span>
-            ))}
-        </span>
-    );
-};
+        ))}
+    </span>
+;
 
 export default RenderColor;
